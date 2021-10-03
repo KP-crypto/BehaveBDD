d3055f11b00a396b443eced52ef9d6f97ecb78c0
Ketan Puranik <69633773+KP-crypto@users.noreply.github.com>
2021-10-03 12:29:34 +0530

    Update OrangeHRMsteps.py

--- features/steps/OrangeHRMsteps.py
+++ features/steps/OrangeHRMsteps.py
@@ -1,9 +1,16 @@
 from behave import *
 from selenium import webdriver
+from webdriver-manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.option import Options
+
+chrome_option = Options()
+chrome_option.add_argument('--hedless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 @given(u'launch Chrome Browser')
 def launchBrowser(context):
-    context.driver=webdriver.Chrome(executable_path="C:/Users/Acer/Downloads/chromedriver_win32/chromedriver.exe")
+    context.driver=webdriver.Chrome(ChromeDriverManager().install(),options=chrome_option)
 
 @when(u'User is on HomePage')
 def OpenHomePage(context):
